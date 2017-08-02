6b9446688fd17ca703d660572fce14209dab46cd
shg@dsi <akter.hossain@dsinnovators.com>
2017-08-02 19:32:58 +0600

    #warmup https://www.hackerrank.com/challenges/birthday-cake-candles

--- hackerrank/src/main/java/Solution.java
+++ hackerrank/src/main/java/Solution.java
@@ -6,16 +6,27 @@ import java.util.regex.*;
 
 public class Solution {
 
+    static int birthdayCakeCandles(int n, int[] ar) {
+        int count = 0, tallest = Integer.MIN_VALUE;
+        for (int height : ar) {
+            if(height > tallest) {
+                tallest = height;
+                count = 1;
+            } else if (height == tallest) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int el, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        long total = 0;
-        for (int i = 0; i < 5; i++) {
-            el = in.nextInt();
-            if (el < min) min = el;
-            if (el > max) max = el;
-            total += el;
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
         }
-        System.out.println((total - max) + " " + (total - min));
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
     }
 }
