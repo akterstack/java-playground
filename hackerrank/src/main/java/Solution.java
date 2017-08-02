2f96c27ebdd015bb23baec915f384204cb8fa291
shg@dsi <akter.hossain@dsinnovators.com>
2017-08-02 12:30:25 +0600

    Solution for https://www.hackerrank.com/challenges/diagonal-difference/problem

--- hackerrank/src/main/java/Solution.java
+++ hackerrank/src/main/java/Solution.java
@@ -6,5 +6,20 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
+        Scanner scanner = new Scanner(System.in);
+        int dim = scanner.nextInt();
+        int[][] matrix = new int[dim][dim];
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        int a = 0, b = 0;
+        for (int i = 0, j = 0, k = dim -1; i < dim; i++, j++, k--) {
+            a += matrix[i][j];
+            b += matrix[i][k];
+        }
+        System.out.println(Math.abs(a - b));
     }
+
 }
