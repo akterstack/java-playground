da9d0b7b6fd97a476be82b4132c0885f7cbb25ce
shg@dsi <akter.hossain@dsinnovators.com>
2017-08-02 16:02:58 +0600

    #warmup https://www.hackerrank.com/challenges/mini-max-sum

--- hackerrank/src/main/java/Solution.java
+++ hackerrank/src/main/java/Solution.java
@@ -1,24 +1,21 @@
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt(), pad = size - 1, hash = size - pad;
-        while (pad >= 0) {
-            for (int p = pad; p > 0; p--) {
-                System.out.print(" ");
-            }
-            for (int h = 0; h < hash; h++) {
-                System.out.print("#");
-            }
-            System.out.println();
-            pad--;
-            hash++;
+        Scanner in = new Scanner(System.in);
+        int el, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        long total = 0;
+        for (int i = 0; i < 5; i++) {
+            el = in.nextInt();
+            if (el < min) min = el;
+            if (el > max) max = el;
+            total += el;
         }
+        System.out.println((total - max) + " " + (total - min));
     }
-
 }
